1b86a14ddcc9204437a7c7951f81d24ebf58e213
alicj <alicj@ALICJADIGITAL>
2025-03-13 13:56:53 +0100

    Add project files.

---
+++ uppgift Array week 11/Program.cs
@@ -0,0 +1,25 @@
+﻿//Create empty list of products
+string[] productArray = new string[0];
+int index = 0;
+//Create loop asking user to add the product
+while (true)
+{
+    Console.WriteLine("Input product: ");
+    string data = Console.ReadLine();
+    //If user input exit, break the loop, change to small leters, remove spaces
+    if (data.ToLower().Trim() == "exit")
+    {
+        break;
+    }
+    //Expend the array with one product
+    Array.Resize(ref productArray, productArray.Length + 1);
+    productArray[index] = data;
+    index++;
+}
+//Print the list of products
+Console.WriteLine("Product list: ");
+foreach (var item in productArray)
+{
+    Console.WriteLine(item);
+}
+
